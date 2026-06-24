6118117920a60f6de6a5b59f9115cdc0eb79dd0c
kikor7 <kiko2002@hotmail.es>
2026-06-25 01:45:59 +0200

    creacion

---
+++ portafolio/src/App.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import {
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuRoot,
+  NavigationMenuSub,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from 'radix-vue'
+</script>
+
+<template>
+  <NavigationMenuRoot>
+    <NavigationMenuList>
+      <NavigationMenuItem>
+        <NavigationMenuTrigger />
+        <NavigationMenuContent>
+          <NavigationMenuLink />
+        </NavigationMenuContent>
+      </NavigationMenuItem>
+
+      <NavigationMenuItem>
+        <NavigationMenuLink />
+      </NavigationMenuItem>
+
+      <NavigationMenuItem>
+        <NavigationMenuTrigger />
+        <NavigationMenuContent>
+          <NavigationMenuSub>
+            <NavigationMenuList />
+            <NavigationMenuViewport />
+          </NavigationMenuSub>
+        </NavigationMenuContent>
+      </NavigationMenuItem>
+
+      <NavigationMenuIndicator />
+    </NavigationMenuList>
+
+    <NavigationMenuViewport />
+  </NavigationMenuRoot>
+</template>
